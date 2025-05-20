13e8ddcd2d2720acc47a21a0f8b2ba60e94ca966
Harry Huang <harryhuang2652@qq.com>
2025-05-20 20:02:58 +0800

    style: code style

--- src/utils/Randomness.py
+++ src/utils/Randomness.py
@@ -9,9 +9,7 @@ _T = TypeVar("_T")
 
 class Randomness:
     @staticmethod
-    def about(
-        base: float, max_noise: float = 1.0, min_value: Optional[float] = 0.0, max_value: Optional[float] = None
-    ):
+    def about(base: float, max_noise: float = 1.0, min_value: Optional[float] = 0.0, max_value: Optional[float] = None):
         if min_value is not None and max_value is not None and min_value > max_value:
             raise ValueError("min_value cannot be larger than max_value")
         value = base + (random.random() - 0.5) * 2.0 * max_noise
